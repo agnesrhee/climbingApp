c4f844469bc7b3eb64ca5e4ab8fff73f9b0ecc53
Agnes <agnes.h.rhee@gmail.com>
2026-02-12 12:53:57 -0500

    got swagger working

--- climbing/obj/Debug/net10.0/climbing.AssemblyInfo.cs
+++ climbing/obj/Debug/net10.0/climbing.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("climbing")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+39db194907614e0d4fe5a468420b3a30e5af53f5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ea84d5f0a59e9403e6ef955265f547eb4374598")]
 [assembly: System.Reflection.AssemblyProductAttribute("climbing")]
 [assembly: System.Reflection.AssemblyTitleAttribute("climbing")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
